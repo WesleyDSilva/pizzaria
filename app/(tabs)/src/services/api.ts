3ebc3d8fe4dab93b99e5f4b8ef68fbb9655647fe
WesleyDSilva <wdias00@gmail.com>
2024-08-30 18:28:50 -0300

    summary

--- app/(tabs)/src/services/api.ts
+++ app/(tabs)/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.172:8082'
+    //baseURL: 'http://192.168.0.172:8082'
+    //baseURL: 'http://localhost:8081'
+    baseURL: 'http:// 192.168.0.172:3333'
+   
 })
 
 export{api};
